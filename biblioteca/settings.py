16a3d4f7abc06821571b7f1b99e31c530c769552
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2025-11-04 20:30:05 -0300

    Update settings.py

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e)3**_2b%g!syuxe^u*lb3h4=0b+mh2p0sw43c4gqgwhpgm903
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [*,'your-app-name.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://biblioteca-escolar-web.onrender.com', 'localhost', '127.0.0.1']
 if not DEBUG:
     ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
